0b050365ee86443220301cc4b655f1999834760c
Dmitry Barannik <d.v.barannik@gmail.com>
2021-10-01 18:07:55 +0300

    main.kt: use

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val results = mutableListOf<File>()
+    Algorithms.searchFiles(File("src"), "main.kt", results)
+    val map = Algorithms.groupByParent(results, 2)
+    map.forEach { t, u ->
+        u.forEach { FileUtils.copy(it, File("copies", t.name)) }
+    }
 }
