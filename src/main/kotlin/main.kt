c7efbc5ebbc4d202cac917bb34bd63e14d8ee08e
Dmitry Barannik <d.v.barannik@gmail.com>
2021-10-01 18:28:18 +0300

    main.kt: updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,14 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
+    println("args = ${args.toList()}")
+    val searchIn = File(args[0])
+    val name = args[1]
+    val copyTo = args[2]
+    val parentLvl = args[3].toInt()
     val results = mutableListOf<File>()
-    Algorithms.searchFiles(File("src"), "main.kt", results)
-    val map = Algorithms.groupByParent(results, 2)
-    map.forEach { t, u ->
-        u.forEach { FileUtils.copy(it, File("copies", t.name)) }
-    }
+
+    Algorithms.searchFiles(searchIn, name, results)
+    val map = Algorithms.groupByParent(results, parentLvl)
+    map.forEach { t, u -> u.forEach { FileUtils.copy(it, File(copyTo, t.name)) } }
 }
