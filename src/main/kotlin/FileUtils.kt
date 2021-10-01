52de029f848011820817974428d61292052e3614
Dmitry Barannik <d.v.barannik@gmail.com>
2021-10-01 17:50:44 +0300

    FileUtils.kt: copy

--- src/main/kotlin/FileUtils.kt
+++ src/main/kotlin/FileUtils.kt
@@ -3,6 +3,7 @@ import java.io.File
 object FileUtils {
 
     fun copy(file: File, targetDir: File, namePrefix: String? = null) {
+        targetDir.mkdirs()
         val newFile = File(targetDir.absolutePath, namePrefix.orEmpty() + file.name)
         newFile.createNewFile()
         file.copyTo(newFile, true)
