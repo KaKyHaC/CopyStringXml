8ef0585ad59f9b0dd22ad66f215daf82ed1d5cea
Dmitry Barannik <d.v.barannik@gmail.com>
2021-10-01 18:39:01 +0300

    main.kt: updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,22 +2,26 @@ import java.io.Console
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("args = ${args.toList()}")
+    try {
+        println("args = ${args.toList()}")
 
-    println("searchIn")
-    val searchIn = File(System.console().readLine())
-    println("name")
-    val name = System.console().readLine()
-    println("copyTo")
-    val copyTo = System.console().readLine()
-    println("parentLvl ")
-    val parentLvl = System.console().readLine().toInt()
+        println("searchIn")
+        val searchIn = File(System.console().readLine())
+        println("name")
+        val name = System.console().readLine()
+        println("copyTo")
+        val copyTo = System.console().readLine()
+        println("parentLvl ")
+        val parentLvl = System.console().readLine().toInt()
 
-    val results = mutableListOf<File>()
-    Algorithms.searchFiles(searchIn, name, results)
-    println("results = $results")
-    val map = Algorithms.groupByParent(results, parentLvl)
-    println("map = $map")
-    map.forEach { t, u -> u.forEach { FileUtils.copy(it, File(copyTo, t.name)) } }
-    println("done")
+        val results = mutableListOf<File>()
+        Algorithms.searchFiles(searchIn, name, results)
+        println("results = $results")
+        val map = Algorithms.groupByParent(results, parentLvl)
+        println("map = $map")
+        map.forEach { t, u -> u.forEach { FileUtils.copy(it, File(copyTo, t.name)) } }
+        println("done")
+    } catch (e: Exception) {
+        println("error = $e")
+    }
 }
