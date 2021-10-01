5ba88c58c242901ea17eb17e2a78f05ded07075e
Dmitry Barannik <d.v.barannik@gmail.com>
2021-10-01 18:31:33 +0300

    main.kt: updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,18 @@
+import java.io.Console
 import java.io.File
 
 fun main(args: Array<String>) {
     println("args = ${args.toList()}")
-    val searchIn = File(args[0])
-    val name = args[1]
-    val copyTo = args[2]
-    val parentLvl = args[3].toInt()
+
+    println("searchIn")
+    val searchIn = File(System.console().readLine())
+    println("name")
+    val name = System.console().readLine()
+    println("copyTo")
+    val copyTo = System.console().readLine()
+    println("parentLvl ")
+    val parentLvl = System.console().readLine().toInt()
+
     val results = mutableListOf<File>()
 
     Algorithms.searchFiles(searchIn, name, results)
