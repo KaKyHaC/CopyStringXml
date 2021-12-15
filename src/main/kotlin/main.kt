588572ac92942f5b94353e0657b1758057f194c5
Dmitry Barannik <d.v.barannik@gmail.com>
2021-12-15 13:01:18 +0200

    main.kt: modeGather

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,29 @@ import java.io.File
 
 fun main(args: Array<String>) {
     println("args = ${args.toList()}")
-    println("gather - 1, broadcast - 2")
+    println("gather - 1,  broadcastStringsLocalizations - 2")
     when(System.console().readLine().toIntOrNull()) {
         1 -> modeGather()
+        2 ->  modeBroadcastStringsLocalizations()
         else -> Unit
+    }
+}
+
+fun modeBroadcastStringsLocalizations() {
+    try {
+        println("copyFrom")
+        val originFile = File(System.console().readLine())
+        println("copyTo")
+        val copyTo = File(System.console().readLine())
+        println("start")
+
+        val stringFiles = mutableListOf<File>()
+        Algorithms.searchFiles(originFile, ".xml", stringFiles)
+        val mapModuleStrings = Algorithms.groupByParent(stringFiles, 1)
+
+        println("done")
+    } catch (e: Exception) {
+        println("error = $e")
     }
 }
 
