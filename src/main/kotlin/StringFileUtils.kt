2377872d3a5e38a1dbc7f0360842c37d5764a238
Dmitry Barannik <d.v.barannik@gmail.com>
2022-01-06 14:28:37 +0200

    StringFileUtils.kt: encapsulateApostrophe

--- src/main/kotlin/StringFileUtils.kt
+++ src/main/kotlin/StringFileUtils.kt
@@ -3,6 +3,17 @@ import java.io.File
 object StringFileUtils {
 
     fun encapsulateApostrophe(file: File) {
+        file.writeText(encapsulateApostrophe(file.readText()))
+    }
 
+    fun encapsulateApostrophe(text: String): String {
+        val buffer = StringBuffer(text)
+        buffer.forEachIndexed { index, c ->
+            if (c == '\'' && buffer[index - 1] != '\\') {
+                println("$c at $index")
+                buffer.insert(index, '\\')
+            }
+        }
+        return buffer.toString()
     }
 }
